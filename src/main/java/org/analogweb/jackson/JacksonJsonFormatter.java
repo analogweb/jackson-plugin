246facc09bde32521bfc390e73a918eab3cd5646
snowgoose.yk <snowgoose.yk@gmail.com>
2012-05-21 19:03:17 +0900

    Add Formatter.

--- src/main/java/org/analogweb/jackson/JacksonJsonFormatter.java
+++ src/main/java/org/analogweb/jackson/JacksonJsonFormatter.java
@@ -6,21 +6,41 @@ import org.analogweb.DirectionFormatter;
 import org.analogweb.RequestContext;
 import org.analogweb.exception.FormatFailureException;
 import org.codehaus.jackson.map.ObjectMapper;
+import org.codehaus.jackson.map.SerializationConfig.Feature;
 
 /**
  * @author snowgoose
  */
 public class JacksonJsonFormatter implements DirectionFormatter {
+    
+    private ObjectMapper mapper;
+
+    public JacksonJsonFormatter(){
+        this.mapper = initObjectMapper();
+    }
+    
+    protected ObjectMapper initObjectMapper(){
+        ObjectMapper newMapper = new ObjectMapper();
+        newMapper.configure(Feature.FAIL_ON_EMPTY_BEANS, false);
+        return newMapper;
+    }
 
     @Override
     public void formatAndWriteInto(RequestContext writeTo, String charset, Object source) {
-        // TODO Auto-generated method stub
-        ObjectMapper mapper = new ObjectMapper();
+        ObjectMapper mapper = getObjectMapper();
         try {
             mapper.writeValue(writeTo.getResponse().getOutputStream(), source);
         } catch (IOException e) {
             throw new FormatFailureException(e, source, getClass().getName());
         }
+    }
+    
+    protected ObjectMapper getObjectMapper(){
+        return this.mapper;
+    }
+
+    public synchronized void setObjectMapper(ObjectMapper mapper) {
+        this.mapper = mapper;
     }
 
 }
--- src/test/java/org/analogweb/jackson/JacksonJsonFormatterTest.java
+++ src/test/java/org/analogweb/jackson/JacksonJsonFormatterTest.java
@@ -1,52 +1,79 @@
 package org.analogweb.jackson;
 
-import static org.junit.Assert.fail;
-import static org.mockito.Mockito.mock;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
 import java.text.SimpleDateFormat;
-import java.util.Date;
+
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletResponse;
 
 import org.analogweb.RequestContext;
+import org.analogweb.exception.FormatFailureException;
+import org.codehaus.jackson.JsonGenerationException;
+import org.codehaus.jackson.map.JsonMappingException;
+import org.codehaus.jackson.map.ObjectMapper;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 public class JacksonJsonFormatterTest {
 
     private JacksonJsonFormatter formatter;
     private RequestContext context;
+    private HttpServletResponse response;
+    
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
 
     @Before
     public void setUp() throws Exception {
         formatter = new JacksonJsonFormatter();
         context = mock(RequestContext.class);
+        response = mock(HttpServletResponse.class);
+        when(context.getResponse()).thenReturn(response);
     }
 
     @Test
-    public void test() throws Exception {
+    public void testFormatAndWriteInto() throws Exception {
+        final ByteArrayOutputStream out = new ByteArrayOutputStream();
+        when(response.getOutputStream()).thenReturn(new ServletOutputStream() {
+            @Override
+            public void write(int arg0) throws IOException {
+                out.write(arg0);
+            }
+        });
         Bean source = new Bean("snowgoose",true,new SimpleDateFormat("yyyy/MM/dd").parse("1978/4/20"));
         formatter.formatAndWriteInto(context, "UTF-8", source);
-        fail("Not yet implemented");
+        String actual = new String(out.toByteArray(),"UTF-8");
+        assertThat(actual,is("{\"name\":\"snowgoose\",\"alive\":true,\"date\":261846000000}"));
     }
 
-    private static class Bean {
-        private String name;
-        private boolean alive;
-        private Date date;
-        public Bean(String name, boolean alive, Date date) {
-            super();
-            this.name = name;
-            this.alive = alive;
-            this.date = date;
-        }
-        public String getName() {
-            return name;
-        }
-        public boolean isAlive() {
-            return alive;
-        }
-        public Date getDate() {
-            return date;
-        }
-        
+    @Test
+    public void testFormatAndWriteIntoOccursIOException() throws Exception {
+        thrown.expect(FormatFailureException.class);
+        final ByteArrayOutputStream out = new ByteArrayOutputStream();
+        when(response.getOutputStream()).thenReturn(new ServletOutputStream() {
+            @Override
+            public void write(int arg0) throws IOException {
+                out.write(arg0);
+            }
+        });
+        ObjectMapper alwaysIOError = new ObjectMapper(){
+            @Override
+            public void writeValue(OutputStream out, Object value) throws IOException,
+                    JsonGenerationException, JsonMappingException {
+                throw new IOException();
+            }
+        };
+        Bean source = new Bean("snowgoose",true,new SimpleDateFormat("yyyy/MM/dd").parse("1978/4/20"));
+        formatter.setObjectMapper(alwaysIOError);
+        formatter.formatAndWriteInto(context, "UTF-8", source);
     }
+
 }
