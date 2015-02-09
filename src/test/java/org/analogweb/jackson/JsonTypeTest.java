815e3e228ce24ab10b22e42a9a9492d893ff5d84
snowgooseyk <snowgoose.yk@gmail.com>
2015-02-09 23:41:14 +0900

    Fix some issue.

--- src/test/java/org/analogweb/jackson/JsonTypeTest.java
+++ src/test/java/org/analogweb/jackson/JsonTypeTest.java
@@ -13,36 +13,54 @@ import org.analogweb.annotation.Post;
 import org.analogweb.annotation.RequestFormats;
 import org.analogweb.annotation.Route;
 import org.analogweb.core.MediaTypes;
+import org.analogweb.core.fake.FakeApplication;
 import org.analogweb.core.fake.ResponseResult;
 import org.analogweb.util.Maps;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
-
 
 /**
  * @author snowgooseyk
  */
 @Route
 public class JsonTypeTest {
-    
+
+    private FakeApplication app;
+
+    @Before
+    public void setUp() {
+        app = fakeApplication();
+    }
+
+    @After
+    public void tearDown() {
+        app.shutdown();
+    }
+
     @Route
     @RequestFormats(MediaTypes.APPLICATION_JSON)
     @Post
-    public String test(@JsonType Bean bean){
+    public String test(@JsonType Bean bean) {
         return bean.getName();
     }
-    
+
     @Test
-    public void testOk(){
-        Map<String, List<String>> header = Maps.newHashMap("Content-Type", Arrays.asList("application/json"));
-        ResponseResult result = fakeApplication().request("jsontypetest/test", "POST",header, new ByteArrayInputStream("{\"name\":\"snowgooseyk\"}".getBytes()));
-        assertThat(result.getStatus(),is(200));
+    public void testOk() {
+        Map<String, List<String>> header = Maps.newHashMap("Content-Type",
+                Arrays.asList("application/json"));
+        ResponseResult result = app.request("jsontypetest/test", "POST", header,
+                new ByteArrayInputStream("{\"name\":\"snowgooseyk\"}".getBytes()));
+        assertThat(result.getStatus(), is(200));
         assertThat(result.getResponseBody().toString(), is("snowgooseyk"));
     }
 
     @Test
-    public void testUnsupportedMediaType(){
-        Map<String, List<String>> header = Maps.newHashMap("Content-Type", Arrays.asList("plain/text"));
-        ResponseResult result = fakeApplication().request("jsontypetest/test", "POST",header, new ByteArrayInputStream("{\"name\":\"snowgooseyk\"}".getBytes()));
-        assertThat(result.getStatus(),is(415));
+    public void testUnsupportedMediaType() {
+        Map<String, List<String>> header = Maps.newHashMap("Content-Type",
+                Arrays.asList("plain/text"));
+        ResponseResult result = app.request("jsontypetest/test", "POST", header,
+                new ByteArrayInputStream("{\"name\":\"snowgooseyk\"}".getBytes()));
+        assertThat(result.getStatus(), is(415));
     }
 }
