bc094a391942af8c286e47e3c320c07953fccab7
snowgooseyk <snowgoose.yk@gmail.com>
2015-09-06 23:01:46 +0900

    Became compatible for 0.9.9

--- src/test/java/org/analogweb/jackson/JacksonJsonFormatterTest.java
+++ src/test/java/org/analogweb/jackson/JacksonJsonFormatterTest.java
@@ -3,7 +3,6 @@ package org.analogweb.jackson;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
@@ -13,8 +12,6 @@ import java.util.Date;
 
 import org.analogweb.RequestContext;
 import org.analogweb.ResponseContext;
-import org.analogweb.ResponseContext.ResponseWriter;
-import org.analogweb.core.DefaultResponseWriter;
 import org.analogweb.core.FormatFailureException;
 import org.junit.Before;
 import org.junit.Rule;
@@ -44,8 +41,6 @@ public class JacksonJsonFormatterTest {
     @Test
     public void testFormatAndWriteInto() throws Exception {
         final ByteArrayOutputStream out = new ByteArrayOutputStream();
-        ResponseWriter writer = new DefaultResponseWriter();
-        when(response.getResponseWriter()).thenReturn(writer);
         Date expectedDate = new SimpleDateFormat("yyyy/MM/dd").parse("1978/4/20");
         Bean source = new Bean("snowgoose", true, expectedDate);
         formatter.formatAndWriteInto(context, response, "UTF-8", source).writeInto(out);
@@ -60,8 +55,6 @@ public class JacksonJsonFormatterTest {
     public void testFormatAndWriteIntoOccursIOException() throws Exception {
         thrown.expect(FormatFailureException.class);
         final ByteArrayOutputStream out = new ByteArrayOutputStream();
-        ResponseWriter writer = new DefaultResponseWriter();
-        when(response.getResponseWriter()).thenReturn(writer);
         ObjectMapper alwaysIOError = new ObjectMapper() {
 			private static final long serialVersionUID = 1L;
 
